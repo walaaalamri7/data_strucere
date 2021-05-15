84c0f717759695267a51bfe796738d9e05ce8623
walaaalamri7 <74015810+walaaalamri7@users.noreply.github.com>
2021-05-15 21:41:41 +0300

    Add files via upload

---
+++ lab5.3.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lab5_3
+//
+//  Created by MAC on 09/07/1442 AH.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter num :" ;
+    cin>>n;
+    
+    cout<<"the result of adding n number : ";
+    return 0;
+}
